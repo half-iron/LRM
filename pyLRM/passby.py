6b9af5fed8f738a13065774cb18e0f005e3d26bc
e-sr <enzo.scossa.romano@gmail.com>
2018-11-12 22:00:09 +0100

    passby name modification

---
+++ pyLRM/passby.py
@@ -0,0 +1,129 @@
+import datetime
+import json
+from pyLRM.config import xbee_axle_sensors_name_from_addr,xbee_axle_sensors
+
+class TrainPassby(object):
+    DICT_KEYS=set(["stop_rec","start_rec","ax_data", "trigger",'sync_time'])
+    def __init__(self,axle_sensors_names, stop_delay=10, ax_counter_low_err=4):
+        self.ax_names= axle_sensors_names
+        self._ax_counter={name:0 for name in self.ax_names}
+        self._ax_data =[]# {name: {} for name in self.ax_names}
+        #
+        self.stop_delay=datetime.timedelta(seconds=stop_delay)
+        self._stopped=False
+        self._start_after_ax=2
+        #
+        self._ax_counter_low_err=ax_counter_low_err
+        self.errors=[]
+        self._d = {
+                   "trigger": None,
+                   "start_rec":None,
+                   "stop_delay":stop_delay,
+                   "start_after_ax":self._start_after_ax,
+                   }
+        #'dict' object has no attribute 'append'.
+
+        self._start_time=None
+        self._last_ax_timestamp=None
+        self._ax_trigger=None
+
+    @property
+    def is_complete(self):
+        return set(self._d.keys()).issubset(TrainPassby.DICT_KEYS)
+
+    @property
+    def is_error(self):
+        return len(self.errors)>0
+
+    def rec(self):
+        ax1_count, ax2_count=[self._ax_counter[n] for n in self.ax_names]
+        if self._stopped or self.is_error:
+            return False
+        elif self._start_time is None:
+            if ax1_count==self._start_after_ax:
+                if ax2_count==0:
+                    self._start_time=self._now()
+                    self._ax_trigger=self.ax_names[0]
+                    return True
+                else:
+                    self.add_error('error1')
+                    return False
+            elif ax2_count==self._start_after_ax:
+                if ax1_count==0:
+                    self._start_time=self._now()
+                    self._ax_trigger=self.ax_names[1]
+                    return True
+                else:
+                    self.add_error('error2')
+                    return False
+            else:
+                return False
+        elif (self._last_ax_timestamp+ self.stop_delay<=self._now()):
+            for n in self.ax_names:
+                if self._ax_counter[n]<self._ax_counter_low_err:
+                    self.add_error('{}_number_low_error'.format(n))
+            self._stopped=True
+            return False
+        else:
+            return True
+
+    @property
+    def _name(self):
+        try:
+            name= "{start_rec:%Y_%m_%d_%Hh%Mm%Ss}_passby".format(**self._d)
+        except TypeError:
+            name = "{:%Y_%m_%d_%Hh%Mm%Ss}_passby_no_start".format(self._now())
+        if len(self.errors):
+            name = name + "_ERR"
+        return name
+
+    def add_axle_data(self,ax_name,header, time_wheel_on, time_wheel_off, timestamp,**kwargs):
+        self._ax_counter[ax_name]+=1
+        self._ax_data.append([timestamp, self._ax_counter[ax_name], ax_name,time_wheel_off, time_wheel_on, header])
+        self._last_ax_timestamp=timestamp
+
+        if header=="MSG_HEADER_AXLE_ERROR":
+            self.errors.append((ax_name,timestamp))
+
+    def add_error(self,err="timeout"):
+        self.errors.append((err,self._now()))
+
+    def set_rec_stop_time(self):
+        self._d['stop_rec']= self._now()
+    def set_rec_start_time(self):
+        self._d['start_rec']= self._now()
+
+    def set_xl2_BBG_sync_time(self, xl2time):
+        self._d['sync_time']= {"xl2":xl2time,"BBG":self._now()}
+
+    def _now(self):
+        return datetime.datetime.now()
+
+    def export(self,path):
+        filePath = path.joinpath(self._name+".json")
+        self._d['errors']=self.errors
+        self._d['ax_data'] = self._ax_data
+        self._d['trigger'] = self._ax_trigger
+        self._d['ax_counts'] = self._ax_counter
+        with filePath.open('w') as f:
+            json.dump(self._d, f,indent=2,sort_keys=True,default=str)
+
+
+def sync_str_to_datetime(s):
+    try:
+        t = datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f")
+    except ValueError:
+        t = datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+    return t
+
+def xl2_time_correction(passby):
+    """
+    Berechnet zeitkorrektur für xl2
+
+    xl2_start_uncorrected  ist nur für achsdaten ohne syncronisierung nötig
+    """
+    sync = passby["sync_time"]
+    return sync_str_to_datetime(sync['BBG']) - sync_str_to_datetime(sync['xl2'])
+
+
+
